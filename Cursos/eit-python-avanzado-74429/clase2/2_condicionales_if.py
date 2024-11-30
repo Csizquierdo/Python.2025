9d0674bace4befa991a567cba1b97f19673cfd6e
Csizquierdo <csizquierdo88@gmail.com>
2024-11-30 13:16:19 -0300

    Update Clase 2

---
+++ Cursos/eit-python-avanzado-74429/clase2/2_condicionales_if.py
@@ -0,0 +1,10 @@
+# Condicionales
+contraseña = "admin"
+respuesta = input("Ingresa la contraseña: ")
+
+if respuesta == contraseña:
+    print("Acceso correcto")
+    print("✅")
+else:
+    print("Acceso denegado")
+    print("🚫")
---
+++ Cursos/eit-python-avanzado-74429/clase2/4_condicioanles_if_anidado.py
@@ -0,0 +1,12 @@
+edad = int(input("Edad: "))
+if edad < 13:
+    print("eres niño")
+else:
+    if edad < 18:
+        print("eres adolescente")
+    else:
+        if edad < 65:
+            print("eres adulto")
+        else:
+            if edad < 130:
+                print("eres anciano")
